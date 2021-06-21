2b2b186cd45d44d5cd0630a4e9c8813272559411
Nikonerds <nikonerds@gmail.com>
2021-06-21 18:01:51 +0000

    a

--- a.py
+++ a.py
@@ -1,6 +1,18 @@
-from os import system
-for i in range(10000):
-    with open("a.txt", "a") as f:
-        f.write(str(i))
-    system(f"git add --all && git commit -m {i}")
-    print(i)
+import asyncio
+import aiofiles
+
+async def run():
+    async with aiofiles.open("a.txt", "a") as f:
+        await f.write("a")
+    proc = await asyncio.create_subprocess_shell(
+        "git add --all && git commit -m 'a'",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    await proc.communicate()
+
+async def main():
+    for i in range(1000):
+        tasks = [run() for _ in range(100)]
+        await asyncio.gather(*tasks)
+
+asyncio.run(run())
