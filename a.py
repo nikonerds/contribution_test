4d62cf8007bef07302afa453970b77354bf25db5
Nikonerds <nikonerds@gmail.com>
2021-06-21 18:04:57 +0000

    a

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import aiofiles
 from random import randint
 
 async def run():
-    async with aiofiles.open("a.txt", "a") as f:
+    async with aiofiles.open(f"{randint(100, 100000000)}.txt", "w") as f:
         await f.write("a")
     proc = await asyncio.create_subprocess_shell(
         "git add --all && git commit -m 'a'",
