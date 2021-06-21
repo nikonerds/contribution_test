a7fb018351214d709f92353bd28d29028c1849bc
nikonerds <nikonerds@gmail.com>
2021-06-21 18:36:54 +0000

    a

--- a.py
+++ a.py
@@ -1,18 +1,25 @@
 import asyncio
 import aiofiles
+from random import choice
 
-async def run(i):
-    async with aiofiles.open("a.txt", "a") as f:
-        await f.write("a")
-    proc = await asyncio.create_subprocess_shell(
-        "git add --all && git commit -m 'a'",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+a = [x for x in range(1000)]
+
+async def run_cmd(cmd):
+    proc = await asyncio.create_subprocess_shell(cmd)
     await proc.communicate()
-    print(i)
+
+async def run():
+    async with aiofiles.open("a.txt", "w") as f:
+        await f.write(str(choice(a)))
+    await run_cmd("git add --all && git commit -m 'a'")
 
 async def main():
-    for i in range(1000):
-        await run(i)
+    i = 0
+    for _ in range(100000000000):
+        for __ in range(1000):
+            i += 1
+            await run()
+            print(i)
+        await run_cmd("git push")
 
 asyncio.run(main())
